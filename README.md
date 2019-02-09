15189655c7d52c561a39a138c6bd1b414da40943
FelixWeichselgartner <f.wei3012@gmail.com>
2019-02-09 19:41:27 +0100

    made a start square

---
+++ README.md
@@ -0,0 +1,8 @@
+# Minesweeper clone in Java
+
+A simple Minesweeper clone in Java with no sprites
+
+# What I Learned
+
+* MouseListener in Java
+* Usage of CheckStyle
--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -7,15 +7,37 @@ import java.awt.event.*;
  * main class
  */
 public class Minesweeper extends JPanel {
-    private static int WIDTH = 15, HEIGHT = 15;
-    private static int amountPixels = 40;
-    private static int space = 3;
+    /**
+     * maximum of nodes no x and y coordinate.
+     */
+    private final int WIDTH = 15, HEIGHT = 15;
+    /**
+     * amount of pixels of one node (width and height).
+     */
+    private final int amountPixels = 40;
+    /**
+     * space between 2 nodes.
+     */
+    private final int space = 3;
+    /**
+     * the actual gameBoard.
+     */
     private Node[][] gameBoard = new Node[WIDTH][HEIGHT];
+    /**
+     * the amount of bombs on the current gameBoard.
+     */
     private int amountBombs;
+    /**
+     * keeps track of gameOver or not gameOver.
+     */
     private Boolean gameOver;
+    /**
+     * true if the first move has been made - false if not.
+     */
+    private Boolean initialised = false;
 
     /**
-     * generates a certain amount of random generated bombs
+     * generates a certain amount of random generated bombs.
      */
     public void generateBombs() {
         Random randomNumber = new Random();
@@ -25,7 +47,7 @@ public class Minesweeper extends JPanel {
                 x = randomNumber.nextInt(WIDTH - 1);
                 y = randomNumber.nextInt(HEIGHT - 1);
 
-            } while(gameBoard[x][y].getContent() == 'b');
+            } while (gameBoard[x][y].getContent() == 'b' || gameBoard[x][y].getOpened());
             gameBoard[x][y].setContent('b');
         }
     }
@@ -41,23 +63,21 @@ public class Minesweeper extends JPanel {
         }
         this.gameOver = false;
         this.amountBombs = 64;
-        generateBombs();
     }
 
     /**
-     * paints the gameBoard
-     * red squares for bombs
-     * gray squares for not opened nodes
-     * orange squares for marked nodes
-     * light grey for opened nodes with blue number -> amount of bombs around node
+     * paints the gameBoard.
+     * red squares for bombs.
+     * gray squares for not opened nodes.
+     * orange squares for marked nodes.
+     * light grey for opened nodes with blue number -> amount of bombs around node.
+     *
      * @param g
      */
     public void paint(Graphics g) {
         int constantx = 8;
         int constanty = 10;
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.GREEN);
-        Boolean flag;
+        g.setColor(Color.GREEN);
         g.setFont(new Font("TimesRoman", Font.PLAIN, 30));
         for (int i = 0; i < WIDTH; i++) {
             for (int k = 0; k < HEIGHT; k++) {
@@ -65,21 +85,20 @@ public class Minesweeper extends JPanel {
                     //not opened
                     if (gameBoard[i][k].getMarked()) {
                         g.setColor(Color.orange);
-                        g.fillRect(i*amountPixels+i*space, k*amountPixels+k*space, amountPixels, amountPixels);
+                        g.fillRect(i * amountPixels + i * space, k * amountPixels + k * space, amountPixels, amountPixels);
                     } else {
                         g.setColor(Color.GRAY);
-                        g.fillRect(i * amountPixels+i*space, k * amountPixels+k*space, amountPixels, amountPixels);
+                        g.fillRect(i * amountPixels + i * space, k * amountPixels + k * space, amountPixels, amountPixels);
                     }
                 } else {
                     if (gameBoard[i][k].getContent() == 'e') {
                         g.setColor(Color.LIGHT_GRAY);
-                        g.fillRect(i*amountPixels+i*space, k*amountPixels+k*space, amountPixels, amountPixels);
+                        g.fillRect(i * amountPixels + i * space, k * amountPixels + k * space, amountPixels, amountPixels);
                         g.setColor(Color.BLUE);
-                        g.drawString(Integer.toString(gameBoard[i][k].getAmountBombsNearby()), i*amountPixels+i*space+amountPixels/2-constantx,k*amountPixels+k*space+amountPixels/2+constanty);
-                        //System.out.println(Integer.toString(gameBoard[i][k].getAmountBombsNearby()));
+                        g.drawString(Integer.toString(gameBoard[i][k].getAmountBombsNearby()), i * amountPixels + i * space + amountPixels / 2 - constantx, k * amountPixels + k * space + amountPixels / 2 + constanty);
                     } else if (gameBoard[i][k].getContent() == 'b') {
                         g.setColor(Color.red);
-                        g.fillRect(i*amountPixels+i*space, k*amountPixels+k*space, amountPixels, amountPixels);
+                        g.fillRect(i * amountPixels + i * space, k * amountPixels + k * space, amountPixels, amountPixels);
                     }
                 }
             }
@@ -88,7 +107,8 @@ public class Minesweeper extends JPanel {
 
     /**
      * updates the current window
-     * @param jframe    JFrame from gameloop (main)
+     *
+     * @param jframe JFrame from gameloop (main)
      */
     public void updateWindow(JFrame jframe) {
         jframe.getContentPane().validate();
@@ -104,14 +124,14 @@ public class Minesweeper extends JPanel {
         for (int i = 0; i < WIDTH; i++) {
             for (int k = 0; k < HEIGHT; k++) {
                 temp = 0;
-                if (i>0) {
-                    if (k>0) {
+                if (i > 0) {
+                    if (k > 0) {
                         //above left
                         if (gameBoard[i - 1][k - 1].getContent() == 'b') {
                             temp++;
                         }
                     }
-                    if (k<HEIGHT-1) {
+                    if (k < HEIGHT - 1) {
                         //below left
                         if (gameBoard[i - 1][k + 1].getContent() == 'b') {
                             temp++;
@@ -122,8 +142,8 @@ public class Minesweeper extends JPanel {
                         temp++;
                     }
                 }
-                if (i<WIDTH-1) {
-                    if (k>0) {
+                if (i < WIDTH - 1) {
+                    if (k > 0) {
                         //above right
                         if (gameBoard[i + 1][k - 1].getContent() == 'b') {
                             temp++;
@@ -133,20 +153,20 @@ public class Minesweeper extends JPanel {
                     if (gameBoard[i + 1][k].getContent() == 'b') {
                         temp++;
                     }
-                    if (k<HEIGHT-1) {
+                    if (k < HEIGHT - 1) {
                         //below right
                         if (gameBoard[i + 1][k + 1].getContent() == 'b') {
                             temp++;
                         }
                     }
                 }
-                if (k>0) {
+                if (k > 0) {
                     //above
                     if (gameBoard[i][k - 1].getContent() == 'b') {
                         temp++;
                     }
                 }
-                if (k<HEIGHT-1) {
+                if (k < HEIGHT - 1) {
                     //below
                     if (gameBoard[i][k + 1].getContent() == 'b') {
                         temp++;
@@ -160,7 +180,8 @@ public class Minesweeper extends JPanel {
     /**
      * gameloop with MouseListener
      * breaks out of gameloop if a bomb node is opened
-     * @param frame     JFrame from main
+     *
+     * @param frame JFrame from main
      */
     public void gameloop(JFrame frame) {
         //implement MouseListener
@@ -174,32 +195,41 @@ public class Minesweeper extends JPanel {
 
                 int X = -1, Y = -1;
                 for (int i = 0; i < WIDTH; i++) {
-                    if (mouseX > i*amountPixels+i*space && mouseX < (i+1)*amountPixels+i*space) {
+                    if (mouseX > i * amountPixels + i * space && mouseX < (i + 1) * amountPixels + i * space) {
                         X = i;
                     }
                 }
                 for (int i = 0; i < HEIGHT; i++) {
-                    if (mouseY > i*amountPixels+i*space+30 && mouseY < (i+1)*amountPixels+i*space+30) {
+                    if (mouseY > i * amountPixels + i * space + 30 && mouseY < (i + 1) * amountPixels + i * space + 30) {
                         Y = i;
                     }
                 }
-                if (e.getButton() == MouseEvent.BUTTON1) {
+                if (!initialised) {
                     if (X != -1 && Y != -1) {
                         gameBoard[X][Y].setOpened(true);
-                        if (gameBoard[X][Y].getContent() == 'b') {
-                            gameOver = true;
+                        for (int l = 0; l < 3; l++) {
+                            gameBoard[l + X - 1][Y - 1].setOpened(true);
+                            gameBoard[l + X - 1][Y + 1].setOpened(true);
+                            gameBoard[X - 1][Y].setOpened(true);
+                            gameBoard[X + 1][Y].setOpened(true);
                         }
+                        initialised = true;
                     }
-                    System.out.println("X = " + X + " Y = " + Y);
-                } else if (e.getButton() == MouseEvent.BUTTON3) {
-                    if (X != -1 && Y != -1) {
-                        if (gameBoard[X][Y].getMarked()) {
-                            gameBoard[X][Y].setMarked(false);
-                        } else {
-                            gameBoard[X][Y].setMarked(true);
+                } else {
+                    if (e.getButton() == MouseEvent.BUTTON1) {
+                        if (X != -1 && Y != -1) {
+                            gameBoard[X][Y].setOpened(true);
+                            if (gameBoard[X][Y].getContent() == 'b') {
+                                gameOver = true;
+                            }
                         }
+                        System.out.println("X = " + X + " Y = " + Y);
+                    } else if (e.getButton() == MouseEvent.BUTTON3) {
+                        if (X != -1 && Y != -1) {
+                            gameBoard[X][Y].setMarked(!gameBoard[X][Y].getMarked());
+                        }
+                        System.out.println("X = " + X + " Y = " + Y);
                     }
-                    System.out.println("X = " + X + " Y = " + Y);
                 }
             }
 
@@ -224,32 +254,50 @@ public class Minesweeper extends JPanel {
             }
         });
 
-        calculate();
-        updateWindow(frame);
-
-        while(true) {
+        while (true) {
             updateWindow(frame);
-            System.out.println("running ");
+            if (initialised) {
+                generateBombs();
+                calculate();
+                updateWindow(frame);
+
+                while (true) {
+                    updateWindow(frame);
+                    if (gameOver) {
+                        break;
+                    }
+                }
+            }
             if (gameOver) {
                 break;
             }
         }
-        updateWindow(frame);
+    }
+
+    /**
+     * Dimensions for frame.pack().
+     *
+     * @return the dimensions for the current frame
+     */
+    public Dimension getPreferredSize() {
+        return new Dimension(WIDTH * amountPixels + (WIDTH - 1) * space,
+                HEIGHT * amountPixels + (HEIGHT - 1) * space);
     }
 
     /**
      * main function
      * setting up JFrame and starting gameloop
+     *
      * @param args
      */
     public static void main(String[] args) {
         Minesweeper newGame = new Minesweeper();
 
         JFrame frame = new JFrame("Minesweeper");
-        frame.setSize((WIDTH+1)*amountPixels+(WIDTH-1)*space-23, (HEIGHT+1)*amountPixels+(HEIGHT-1)*space);
-        frame.setResizable(true);
+        frame.add(newGame);
+        frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(newGame);
+        //frame.setResizable(true);
         frame.setVisible(true);
 
         newGame.gameloop(frame);
